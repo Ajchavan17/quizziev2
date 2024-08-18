22842f6aaa555651ba7aebda329ff734d56aca64
Ajit Chavan <ajchavan17@gmail.com>
2024-08-18 16:16:11 +0530

    Added dashboard

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import "./Navbar.css";
+
+const Dashboard = ({ handleLogout }) => {
+  const [selectedItem, setSelectedItem] = useState("Dashboard"); // Default selected item is "Dashboard"
+
+  return (
+    <div className="main-container">
+      <div className="nav-container">
+        <div className="nav-item-1">
+          <h2>QUIZZIE</h2>
+        </div>
+        <div className="nav-item-2">
+          <button
+            className={selectedItem === "Dashboard" ? "selected" : ""}
+            onClick={() => setSelectedItem("Dashboard")}
+          >
+            Dashboard
+          </button>
+          <button
+            className={selectedItem === "Analytics" ? "selected" : ""}
+            onClick={() => setSelectedItem("Analytics")}
+          >
+            Analytics
+          </button>
+          <button
+            className={selectedItem === "Create Quiz" ? "selected" : ""}
+            onClick={() => setSelectedItem("Create Quiz")}
+          >
+            Create Quiz
+          </button>
+        </div>
+        <div className="nav-item-3">
+          <div />
+          <button onClick={handleLogout}>LOGOUT</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,24 @@
-// src/pages/Dashboard/Dashboard.jsx
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.css";
+import Navbar from "../components/Navbar";
 
 const Dashboard = () => {
-  const { logout } = useAuth(); // Access the logout function from AuthContext
-  const navigate = useNavigate(); // useNavigate hook to programmatically navigate
+  const { logout } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
-    logout(); // Call the logout function to clear the authentication state
-    navigate("/login"); // Redirect to the login page after logout
+    logout();
+    navigate("/login");
   };
 
   return (
     <div className="main-container">
       <div className="nav-container">
-        <div className="nav-item-1">
-          <h2>QUIZZIE</h2>
-        </div>
-        <div className="nav-item-2">
-          <button>Dashboard</button>
-          <button>Analytics</button>
-          <button>Create Quiz</button>
-        </div>
-        <div className="nav-item-3"></div>
+        <Navbar handleLogout={handleLogout} />
       </div>
+      <div className="content-container"></div>
     </div>
   );
 };
